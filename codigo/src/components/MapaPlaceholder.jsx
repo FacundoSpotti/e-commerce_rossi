e2a314b91e1a83cf023a6e2224d28e9504467620
FacundoSpotti <facuspotti@gmail.com>
2026-05-26 21:52:59 -0300

    mapita_coso

--- codigo/src/components/MapaPlaceholder.jsx
+++ codigo/src/components/MapaPlaceholder.jsx
@@ -9,34 +9,50 @@ import {
 } from "react-leaflet"
 
 import "leaflet"
+import L from "leaflet";
 import "leaflet/dist/leaflet.css"
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function MapaPlaceholder() {
   return (
     <section className="mapa-bloque">
-      <p className="sobrelinea">Visitanos</p>
-      <h2>Atención con cita previa</h2>
-      <p>
-        El taller se encuentra en Montevideo, Uruguay. Por respeto al espacio de
-        trabajo de Verónica, las visitas se coordinan con cita previa por
-        WhatsApp o correo.
-      </p>
-
+      <div>
+        <p className="sobrelinea">Visitanos</p>
+        <h2>Atención con cita previa</h2>
+        <p>
+          El taller se encuentra en Montevideo, Uruguay. Por respeto al espacio de
+          trabajo de Verónica, las visitas se coordinan con cita previa por
+          WhatsApp o correo.
+        </p>
+    </div>
     <MapContainer
       center={[-34.889791212111895, -56.18002935401105]}
       zoom={13}
       style={{
-        height: "100vh",
+        height: "50vh",
         width: "100%",
       }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a> 
+        &copy; <a href="https://www.stamen.com/">Stamen Design</a> 
+        &copy; OpenStreetMap contributors'
+        url="https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png"
       />
 
         <Marker
-          position={[-34.88, -56.18]}
+          position={[-34.889791212111895, -56.18002935401105]}
           opacity={0.5}
           title="Taller de Rossi"
           zIndexOffset={1000}
